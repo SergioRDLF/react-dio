be1901c5c3de4a4d7f229c954a12da40cad14562
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2022-03-15 11:42:57 -0300

    Refarora o componente item

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,6 +1,6 @@
-const Item = ({ texto }) => {
+const Item = ({ children }) => {
   return (
-    <a href="/" className="list-group-item list-group-item-action list-group-item-dark">{texto}</a>
+    <a href="/" className="list-group-item list-group-item-action list-group-item-dark">{children}</a>
   );
 }
 
