e9b322fcd62f3d7e9f80d2fe11cd39af307dc490
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2022-03-15 11:36:36 -0300

    App React curso dio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return(
+    <>
+      <h1>Aplicação em React</h1>
+      <ul>
+        <li>item1</li>
+        <li>item2</li>
+        <li>item3</li>
+      </ul>
+    </>
   );
 }
 
---
+++ src/components/item/index.js
@@ -0,0 +1,7 @@
+const Item = ({ texto }) => {
+  return (
+    <a href="/" className="list-group-item list-group-item-action list-group-item-dark">{texto}</a>
+  );
+}
+
+export default Item;
